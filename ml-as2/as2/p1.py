8e033ad5b7405daad3106c59c3bb22ef184ea630
cgarciahdez <c.garcia11@uniandes.edu.co>
2016-03-04 17:19:31 -0600

    filter added

--- ml-as2/as2/p1.py
+++ ml-as2/as2/p1.py
@@ -2,9 +2,11 @@ import numpy as np
 
 #
 #This function loads a file with the name given into to np arrays x and y.
-#Parameters: Name of the file to load
+#Parameters: Name of the file to load. Filt : if not -1, filters out only the
+#classes in the given set
 #Returns: nparray x with feature information and nparray y with label information
-def load_file(name):
+def load_file(name, filt=-1):
+    print(filt)
     file = open(name,'r')
     x=[]
     y=[]
@@ -14,19 +16,27 @@ def load_file(name):
             xi=[]
             for i in range(1,len(l)):
                 xi.append(float(l[i]))
-            x.append(xi)
             yi=float(l[-1])
-            if not yi == 0:
-            	y.append(yi)
+            if not yi == 0 and (filt==-1 or yi in filt):
+                y.append(yi)
+                x.append(xi)
     return np.array(x), np.array(y)
 
-def put_files_together():
-	x,y=load_file("./data/%d.csv"%1)
-	for i in range(2,16):
-		xi,yi=load_file("./data/%d.csv"%i)
-		x=np.concatenate((x,xi))
-		y=np.concatenate((y,yi))
-	return x,y
+#
+#This function puts together the results of loading all the data
+#that is split into different participants data
+#Returns: np araay x with feature information and np array y with label information
+def put_files_together(filt=-1):
+    x,y=load_file("./data/%d.csv"%1,filt)
+    for i in range(2,16):
+        xi,yi=load_file("./data/%d.csv"%i,filt)
+        x=np.concatenate((x,xi))
+        y=np.concatenate((y,yi))
+    return x,y
 
-print (put_files_together())
+
+def estimate_parameters_uniGDA():
+    pass
+
+print (put_files_together((1,2)))
 
