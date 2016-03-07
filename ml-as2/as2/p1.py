cf0102159804afde0c47254e8ad92ad989b120c2
cgarciahdez <c.garcia11@uniandes.edu.co>
2016-03-07 01:36:25 -0600

    GAD finished. Cross validation started

--- ml-as2/as2/p1.py
+++ ml-as2/as2/p1.py
@@ -1,6 +1,7 @@
 import numpy as np
 from collections import defaultdict
 from pylab import *
+from math import *
 #
 #This function loads a file with the name given into to np arrays x and y.
 #Parameters: Name of the file to load. Filt : if not -1, filters out only the
@@ -43,9 +44,10 @@ def put_files_together(filt=-1,uni=False):
 #Estimates the mean vector and covariance matrix for each different class
 #in the data
 #Parameters: x-feature dara vector or matrix. y-label data vector
-def estimate_parameters_multiGDA(x,y,k):
+def estimate_parameters_GDA(x,y,k,uni=False):
     mean_vec = defaultdict(lambda: np.ndarray(0))
     covar_matrix= defaultdict(lambda: np.ndarray(0))
+    prior_class=defaultdict(float)
     for j in k:
         ex_number=0
         sum_x=0
@@ -56,48 +58,54 @@ def estimate_parameters_multiGDA(x,y,k):
                 sum_x+=x[i]
 
         mean_vec[j]=sum_x/ex_number
-        print(mean_vec[j])
 
         for i in range(len(y)):
             if j==y[i]:
                 temp=x[i]-mean_vec[j]
                 sum_var+=np.outer(temp,temp)
 
-        covar_matrix[j]=sum_var/ex_number
+        covar_matrix[j] = sum_var/ex_number
+        prior_class[j] = ex_number/len(x)
 
-    return dict(mean_vec), dict(covar_matrix)
+    mean_vec=dict(mean_vec)
+    covar_matrix=dict(covar_matrix)
+
+    if uni:
+        for k in mean_vec:
+            mean_vec[k] = mean_vec[k][0]
+        for k in covar_matrix:
+            covar_matrix[k] = covar_matrix[k][0][0]
+
+    return mean_vec, covar_matrix, dict(prior_class)
 
 #
-#Estimates the mean vector and covariance matrix for each different class
-#in the data
-#Parameters: x-feature dara vector or matrix. y-label data vector
-def estimate_parameters_uniGDA(x,y,k):
-    mean_vec = defaultdict(lambda: np.ndarray(0))
-    covar_matrix= defaultdict(lambda: np.ndarray(0))
+#Calculates all the membership fuctions for each label j in k
+#and returns the j belonging to the class with the highest 
+#g value.
+def g(x,mean,covar,priori,k,uni=False):
+    g_j = defaultdict(float)
     for j in k:
-        ex_number=0
-        sum_x=0
-        sum_var=0
-        for i in range(len(y)):
-            if j==y[i]:
-                ex_number+=1
-                sum_x+=x[i]
+        if uni:
+            g_j[j]=-math.log10(covar[j])-(x-mean[j])**2/(2*covar[j]**2)+math.log10(priori[j])
+        else:
+            g_j[j]=-math.log10(np.linalg.det(covar[j]))-(1/2)*dot(dot((x-mean[j]).T,covar[j]),(x-mean[j]))+math.log10(priori[j])
+    return sorted(g_j)[0]
 
-        mean_vec[j]=sum_x/ex_number
-        print(mean_vec[j])
+def cross_validation(x,y,k=10):
 
-        for i in range(len(y)):
-            if j==y[i]:
-                temp=x[i]-mean_vec[j]
-                sum_var+=np.outer(temp,temp)
-
-        covar_matrix[j]=sum_var/ex_number
-
-    return dict(mean_vec), dict(covar_matrix)
+#
+#Computes confusion matrix given
+def confusion_matrix():
 
 
-    
 
-x,y,k=put_files_together((1,2),True)
-print(estimate_parameters_multiGDA(x,y,k))
+        
+
+
+
+
+x,y,k=put_files_together((1,2))
+mean,covar,priori=(estimate_parameters_GDA(x,y,k))
+print (g(x[0],mean,covar,priori,k))
+
 
